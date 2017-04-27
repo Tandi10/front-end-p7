14354cc02954f2ad9422186029a49b025e0ea39a
jeffpash <jeffypash@gmail.com>
2017-04-27 14:32:25 +0200

    button inscrire

--- front-end-p7/src/App.js
+++ front-end-p7/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Inscriptionannonceur from  './inscriptionannonceur';
+import Inscriptionlinkedin from './inscriptionlinkedin.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Inscriptionlinkedin/>
         <Inscriptionannonceur/>
       </div>
     );
